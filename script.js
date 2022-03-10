8eda543bbddf90d258ceca5daaf354b816f09ba3
Lucas Rafael <79646846+lukulas@users.noreply.github.com>
2022-03-09 22:52:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,22 @@ function subtracao(){
         }
     }
 }
+
+function divisao(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value == 0){
+        window.alert('Por favor, digite um numero!')
+    } else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            let resultado = n / c
+            let teste = resultado.toFixed(2)
+            item.text = `${n} / ${c} = ${teste}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
