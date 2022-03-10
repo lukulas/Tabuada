2e5c306a9afe9593d45e3adce5bbd7df349ced98
Lucas Rafael <79646846+lukulas@users.noreply.github.com>
2022-03-09 22:36:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function tabuada(){
+function multiplicacao(){
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
     if(num.value == 0){
@@ -9,6 +9,39 @@ function tabuada(){
         for(let c = 1; c <= 10; c++){
             let item = document.createElement('option')
             item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
+function adicao(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value == 0){
+        window.alert('Por favor, digite um numero!')
+    } else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} + ${c} = ${n+c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
+
+function subtracao(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value == 0){
+        window.alert('Por favor, digite um numero!')
+    } else{
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} - ${c} = ${n-c}`
             item.value = `tab${c}`
             tab.appendChild(item)
         }
